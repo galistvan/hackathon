1f1bf9a193321f45cbfbdc90873fb6c2e86cdddc
xcenter <herendilajos@gmail.com>
2014-11-28 22:46:37 +0100

    lol
    lol

--- AI/makeDecision.lua
+++ AI/makeDecision.lua
@@ -1,9 +1,14 @@
 function makeDecision(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon) 
-  local action = ""
+  local action = {}
     if (lengthOfArray(availableWeapons) <= 1) then
-        action="pickUpWeapon"
+        action[1]="pickUpWeapon"
+        action[2]="";
+      elseif(enemyNotInLineOfSight(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon)) then 
+        action[1]="move"
+        action[2]=enemyCoordinates(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon)
       else
-        action="attack"
+        action[1]="attack"
+        action[2]=""
       end
   return action
 end
