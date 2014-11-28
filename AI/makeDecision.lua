b6ea1c683040178635f15e5e2a6ba7b22cd1cb68
xcenter <herendilajos@gmail.com>
2014-11-29 00:01:02 +0100

    asdfg
    adfg

--- AI/makeDecision.lua
+++ AI/makeDecision.lua
@@ -6,13 +6,15 @@ function makeDecision(marine, availableWeapons, availableItems, availableAmmo, n
         action[3]="sprint"
       elseif(enemyInSight(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon) < 0) then 
         action[1]="move"
-        action[2]=enemyCoordinates(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon)
+        action[2]={nearestEnemy.X, nearestEnemy.Y}
         action[3]="advance"
       else
         if(getMaximumRange(marine, availableWeapons, availableAmmo) - enemyInSight(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon) > 3) then  
           action[1]="attack"
-          action[2]=enemyCoordinates(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon)
+          action[2]={nearestEnemy.X, nearestEnemy.Y}
           action[3]="guard"
+        else
+         print("Should move towards and KEEL")
         end
       end
   return action
