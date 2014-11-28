3f8b2c4e243e209347bae674dc6c39196c843e49
Daniel <daniel_fatay@epam.com>
2014-11-29 00:05:48 +0100

    Enemy in sight

---
+++ AI/enemyInSight.lua
@@ -0,0 +1,11 @@
+function enemyInSight(marine, availableWeapons, availableItems, availableAmmo, nearestEnemy, nearestWeapon)
+
+	if (Game.Map:entity_has_los(marine.Id, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)) then
+		return -1
+	end
+	attack_path = Game.Map:get_attack_path(marine.Id, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
+	i = #attack_path
+	print("Possible to attack, distance "..i)
+	return i
+end
+
