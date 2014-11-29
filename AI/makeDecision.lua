782259582dfe84bfea35ed438dd2fec89a60f966
xcenter <herendilajos@gmail.com>
2014-11-29 08:55:43 +0100

    FailMarine fix
    fixed failmarine on spawn

--- AI/makeDecision.lua
+++ AI/makeDecision.lua
@@ -3,88 +3,90 @@ require 'Shooter.shoot'
 
 function makeDecision(marine, nearestEnemy, nearestWeapon)
   local action = {}
-  if(marine.Health-marine.Wounds<=2) then
-    action[1]="heal"
-    action[2]=""
-    action[3]="sprint"
-  else
-    if ((lengthOfArray(marine.Inventory) <= 2 or nearestEnemy == nil) or marine.shouldGoForWeapons) then
-      action[1]="pickUpWeapon"
-      action[2]="";
+  if(marine ~= nil) then
+    if(marine.Health-marine.Wounds<=2) then
+      action[1]="heal"
+      action[2]=""
       action[3]="sprint"
-    elseif(enemyInSight(marine, nearestEnemy) <= 0) then
-      if(checkIfMovingOnceEnablesLos(marine, nearestEnemy)) then
-        local movePath = determineAttackPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
-        action[1] = "movetokill"
-        action[2] = {nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-        action[3] = "advance"
-        action[4]=reformMovePath(movePath, marine, nearestEnemy)
-      else
-        -- we can also wait and aim to have a better shot at it or dodge, depending on our health/his weapon
-        local movePath = determineAttackPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
-        if(movePath == -1) then
-          print("Marine: " .. marine.Id .. " has No route to enemy, Going for weapon!", "target", nearestEnemy.Id)
-          action[1]="pickUpWeapon"
-          action[2]="";
-          action[3]="sprint"
-        else
-          action[1]="move"
-          action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-          action[3]="sprint"
-          action[4]=reformMovePath(movePath, marine, nearestEnemy)
-        end
-      end
     else
-      if(getEffectiveRange(marine) > enemyInSight(marine, nearestEnemy)) then
-        if(enemyInSight(marine, nearestEnemy) < 3) then
-          local movePath = determineRetreatPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
-          if(#movePath == 0 and enemyInSight(marine, nearestEnemy) == -1) then
+      if ((lengthOfArray(marine.Inventory) <= 2 or nearestEnemy == nil) or marine.shouldGoForWeapons) then
+        action[1]="pickUpWeapon"
+        action[2]="";
+        action[3]="sprint"
+      elseif(enemyInSight(marine, nearestEnemy) <= 0) then
+        if(checkIfMovingOnceEnablesLos(marine, nearestEnemy)) then
+          local movePath = determineAttackPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
+          action[1] = "movetokill"
+          action[2] = {nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
+          action[3] = "advance"
+          action[4]=reformMovePath(movePath, marine, nearestEnemy)
+        else
+          -- we can also wait and aim to have a better shot at it or dodge, depending on our health/his weapon
+          local movePath = determineAttackPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
+          if(movePath == -1) then
+            print("Marine: " .. marine.Id .. " has No route to enemy, Going for weapon!", "target", nearestEnemy.Id)
             action[1]="pickUpWeapon"
             action[2]="";
             action[3]="sprint"
           else
-            action[1]="backandkill"
+            action[1]="move"
             action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-            action[3]="advance"
+            action[3]="sprint"
             action[4]=reformMovePath(movePath, marine, nearestEnemy)
           end
-        else
-          action[1]="attack"
-          action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-          action[3]="unload"
         end
       else
-        local movePath = determineAttackPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
-        if(#movePath == 0 and enemyInSight(marine, nearestEnemy) == -1) then
-          action[1]="pickUpWeapon"
-          action[2]="";
-          action[3]="sprint"
-        elseif(enemyInSight(marine, self.nearestEnemy) > 0) then
-          action[1]="attack"
-          action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-          action[3]="unload"
-        elseif(Game.Map:cell_has_los(movePath[marine.MovePoints].X, movePath[marine.MovePoints].Y, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y, "ObstaclesAndEntities")) then
-          local finalMovePath = reformMovePath(movePath, marine, nearestEnemy)
-          local finalDestination = finalMovePath[#finalMovePath]
-          if(Game.Map:cell_has_los(finalDestination.X, finalDestination.Y, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y, "ObstaclesAndEntities")) then
-          action[1]="movetokill"
-          action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-          action[3]="advance"
-          action[4]=finalMovePath
+        if(getEffectiveRange(marine) > enemyInSight(marine, nearestEnemy)) then
+          if(enemyInSight(marine, nearestEnemy) < 3) then
+            local movePath = determineRetreatPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
+            if(#movePath == 0 and enemyInSight(marine, nearestEnemy) == -1) then
+              action[1]="pickUpWeapon"
+              action[2]="";
+              action[3]="sprint"
+            else
+              action[1]="backandkill"
+              action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
+              action[3]="advance"
+              action[4]=reformMovePath(movePath, marine, nearestEnemy)
+            end
           else
-          action[1]="moveandaim"
-          action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-          action[3]="ready"
-          action[4]=finalMovePath
+            action[1]="attack"
+            action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
+            action[3]="unload"
           end
         else
-          action[1]="move"
-          action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
-          action[3]="sprint"
-          action[4]=reformMovePath(movePath, marine, nearestEnemy)
+          local movePath = determineAttackPath(marine, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y)
+          if(#movePath == 0 and enemyInSight(marine, nearestEnemy) == -1) then
+            action[1]="pickUpWeapon"
+            action[2]="";
+            action[3]="sprint"
+          elseif(enemyInSight(marine, self.nearestEnemy) > 0) then
+            action[1]="attack"
+            action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
+            action[3]="unload"
+          elseif(Game.Map:cell_has_los(movePath[marine.MovePoints].X, movePath[marine.MovePoints].Y, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y, "ObstaclesAndEntities")) then
+            local finalMovePath = reformMovePath(movePath, marine, nearestEnemy)
+            local finalDestination = finalMovePath[#finalMovePath]
+            if(Game.Map:cell_has_los(finalDestination.X, finalDestination.Y, nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y, "ObstaclesAndEntities")) then
+              action[1]="movetokill"
+              action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
+              action[3]="advance"
+              action[4]=finalMovePath
+            else
+              action[1]="moveandaim"
+              action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
+              action[3]="ready"
+              action[4]=finalMovePath
+            end
+          else
+            action[1]="move"
+            action[2]={nearestEnemy.Bounds.X, nearestEnemy.Bounds.Y}
+            action[3]="sprint"
+            action[4]=reformMovePath(movePath, marine, nearestEnemy)
+          end
+
+
         end
-      
-      
       end
     end
   end
